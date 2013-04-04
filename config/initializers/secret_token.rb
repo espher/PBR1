521cf66b6356d1532e9f8060b682f11ae6518a35
robertosante <robertosante@gmail.com>
2013-04-04 11:20:00 -0700

    Version Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbr::Application.config.secret_token = '937f8a88ed6d2cc8ef6c14a5a0f483feef44701a7f2532fa56fad0898e3ee4ad825603e54d435b76326ac56ec3c19223239f5890f0192cdb33a81cb3d6bd9284'
